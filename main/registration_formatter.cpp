770decfd51f36e5ed28669a7486553dd11abb7de
Dmitriy Shilin <dshil@fastmail.com>
2024-07-17 16:13:34 +0300

    Add FW version to registration data

--- main/registration_formatter.cpp
+++ main/registration_formatter.cpp
@@ -6,18 +6,42 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "registration_formatter.h"
+#include "esp_log.h"
+
+#include "ocs_core/version.h"
+#include "ocs_core/version_to_str.h"
 #include "ocs_iot/network_json_formatter.h"
+#include "registration_formatter.h"
 
 namespace ocs {
 namespace app {
 
+namespace {
+
+const char* log_tag = "registration-formatter";
+
+} // namespace
+
 RegistrationFormatter::RegistrationFormatter(net::BasicNetwork& network) {
+    fanout_formatter_.reset(new (std::nothrow) iot::FanoutJSONFormatter());
+
     network_formatter_.reset(new (std::nothrow) iot::NetworkJSONFormatter(network));
+    fanout_formatter_->add(*network_formatter_);
+
+    core::Version version;
+    if (!version.parse(CONFIG_OCS_CORE_FW_VERSION)) {
+        ESP_LOGE(log_tag, "failed to parse FW version: %s", CONFIG_OCS_CORE_FW_VERSION);
+    }
+
+    core::version_to_str version_str(version);
+
+    version_formatter_.reset(new (std::nothrow) iot::VersionJSONFormatter());
+    version_formatter_->add("version_scs", version_str.c_str());
+    fanout_formatter_->add(*version_formatter_);
 }
 
 void RegistrationFormatter::format(cJSON* json) {
-    network_formatter_->format(json);
+    fanout_formatter_->format(json);
 }
 
 } // namespace app

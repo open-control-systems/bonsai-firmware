1b4efd5f8e5647fffa8fcdf6a7afa4b71b128327
Dmitriy Shilin <dshil@fastmail.com>
2024-09-18 13:09:42 +0200

    Update core

--- components/scs/project_pipeline.h
+++ components/scs/project_pipeline.h
@@ -11,13 +11,13 @@
 #include <memory>
 
 #include "ocs_core/noncopyable.h"
-#include "ocs_iot/http_pipeline.h"
-#include "ocs_iot/json_data_pipeline.h"
-#include "ocs_iot/system_pipeline.h"
 #include "ocs_pipeline/ds18b20/http_handler.h"
-#ifdef CONFIG_OCS_IOT_CONSOLE_PIPELINE_ENABLE
-#include "ocs_iot/console_json_pipeline.h"
-#endif // CONFIG_OCS_IOT_CONSOLE_PIPELINE_ENABLE
+#include "ocs_pipeline/http_pipeline.h"
+#include "ocs_pipeline/json_data_pipeline.h"
+#include "ocs_pipeline/system_pipeline.h"
+#ifdef CONFIG_OCS_PIPELINE_CONSOLE_PIPELINE_ENABLE
+#include "ocs_pipeline/console_json_pipeline.h"
+#endif // CONFIG_OCS_PIPELINE_CONSOLE_PIPELINE_ENABLE
 
 #include "scs/control_pipeline.h"
 
@@ -33,14 +33,14 @@ public:
     status::StatusCode start();
 
 private:
-    std::unique_ptr<iot::SystemPipeline> system_pipeline_;
-    std::unique_ptr<iot::JsonDataPipeline> json_data_pipeline_;
+    std::unique_ptr<pipeline::SystemPipeline> system_pipeline_;
+    std::unique_ptr<pipeline::JsonDataPipeline> json_data_pipeline_;
 
-#ifdef CONFIG_OCS_IOT_CONSOLE_PIPELINE_ENABLE
-    std::unique_ptr<iot::ConsoleJsonPipeline> console_pipeline_;
-#endif // CONFIG_OCS_IOT_CONSOLE_PIPELINE_ENABLE
+#ifdef CONFIG_OCS_PIPELINE_CONSOLE_PIPELINE_ENABLE
+    std::unique_ptr<pipeline::ConsoleJsonPipeline> console_pipeline_;
+#endif // CONFIG_OCS_PIPELINE_CONSOLE_PIPELINE_ENABLE
 
-    std::unique_ptr<iot::HttpPipeline> http_pipeline_;
+    std::unique_ptr<pipeline::HttpPipeline> http_pipeline_;
     std::unique_ptr<ControlPipeline> control_pipeline_;
 
     std::unique_ptr<pipeline::ds18b20::HttpHandler> ds18b20_sensor_http_handler_;

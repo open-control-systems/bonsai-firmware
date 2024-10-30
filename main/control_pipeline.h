67ae0fee9e4a6d890aff00c4556972d03e77a5d8
Dmitriy Shilin <dshil@fastmail.com>
2024-10-30 13:14:47 +0100

    Use flat formatting for all

--- main/control_pipeline.h
+++ main/control_pipeline.h
@@ -33,7 +33,6 @@
 #include "ocs_core/iclock.h"
 #include "ocs_core/noncopyable.h"
 #include "ocs_fmt/json/fanout_formatter.h"
-#include "ocs_fmt/json/field_formatter.h"
 #include "ocs_i2c/istore.h"
 #include "ocs_io/adc_store.h"
 #include "ocs_scheduler/itask_scheduler.h"
@@ -62,25 +61,21 @@ private:
 
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_YL69_ENABLE
     std::unique_ptr<sensor::yl69::RelayPipeline> yl69_sensor_pipeline_;
-    std::unique_ptr<fmt::json::FieldFormatter> yl69_sensor_field_formatter_;
     std::unique_ptr<fmt::json::IFormatter> yl69_sensor_json_formatter_;
 #endif // CONFIG_BONSAI_FIRMWARE_SENSOR_YL69_ENABLE
 
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ENABLE
     std::unique_ptr<sensor::ldr::SensorPipeline> ldr_sensor_pipeline_;
-    std::unique_ptr<fmt::json::FieldFormatter> ldr_sensor_field_formatter_;
     std::unique_ptr<fmt::json::IFormatter> ldr_sensor_json_formatter_;
 #endif // CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ENABLE
 
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_CAPACITIVE_V1_2_ENABLE
     std::unique_ptr<sensor::yl69::DefaultPipeline> capacitive_sensor_sepipeline_;
-    std::unique_ptr<fmt::json::FieldFormatter> capacitive_sensor_field_formatter_;
     std::unique_ptr<fmt::json::IFormatter> capacitive_sensor_json_formatter_;
 #endif // CONFIG_BONSAI_FIRMWARE_SENSOR_CAPACITIVE_V1_2_ENABLE
 
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_SHT41_ENABLE
     std::unique_ptr<sensor::sht41::SensorPipeline> sht41_sensor_pipeline_;
-    std::unique_ptr<fmt::json::FieldFormatter> sht41_sensor_field_formatter_;
     std::unique_ptr<fmt::json::IFormatter> sht41_sensor_json_formatter_;
 #endif // CONFIG_BONSAI_FIRMWARE_SENSOR_SHT41_ENABLE
 
@@ -89,7 +84,6 @@ private:
     std::unique_ptr<sensor::bme280::SpiSensorPipeline> bme280_spi_sensor_pipeline_;
 #endif // CONFIG_BONSAI_FIRMWARE_SENSOR_BME280_SPI_ENABLE
     std::unique_ptr<fmt::json::IFormatter> bme280_sensor_json_formatter_;
-    std::unique_ptr<fmt::json::FieldFormatter> bme280_sensor_field_formatter_;
 #endif // CONFIG_BONSAI_FIRMWARE_SENSOR_BME280_ENABLE
 };
 

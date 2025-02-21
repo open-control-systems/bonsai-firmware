d19b3b6e65c9fd9c2b8d0a72f06091dcb0562272
Dmitriy Shilin <dshil@fastmail.com>
2025-02-21 08:40:01 +0100

    Update control-components

--- projects/bonsai-growlab/main/control_pipeline.h
+++ projects/bonsai-growlab/main/control_pipeline.h
@@ -10,9 +10,9 @@
 
 #include <memory>
 
-#ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ENABLE
-#include "ocs_sensor/ldr/sensor_pipeline.h"
-#endif // CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ENABLE
+#ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ANALOG_ENABLE
+#include "ocs_sensor/ldr/analog_sensor_pipeline.h"
+#endif // CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ANALOG_ENABLE
 
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_BME280_SPI_ENABLE
 #include "ocs_sensor/bme280/spi_sensor_pipeline.h"
@@ -45,10 +45,10 @@ public:
 private:
     std::unique_ptr<io::spi::IStore> spi_master_store_;
 
-#ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ENABLE
-    std::unique_ptr<sensor::ldr::SensorPipeline> ldr_sensor_pipeline_;
+#ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ANALOG_ENABLE
+    std::unique_ptr<sensor::ldr::AnalogSensorPipeline> ldr_sensor_pipeline_;
     std::unique_ptr<fmt::json::IFormatter> ldr_sensor_json_formatter_;
-#endif // CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ENABLE
+#endif // CONFIG_BONSAI_FIRMWARE_SENSOR_LDR_ANALOG_ENABLE
 
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_BME280_ENABLE
 #ifdef CONFIG_BONSAI_FIRMWARE_SENSOR_BME280_SPI_ENABLE
